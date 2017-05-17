b63ada4a3865682fe15048f8b598751f797dda2a
ksikka <kunaal@live.com>
2017-05-17 16:49:24 -0700

    upload website

---
+++ public_html/include/membersite_config.php
@@ -0,0 +1,26 @@
+<?PHP
+require_once("./include/fg_membersite.php");
+
+$fgmembersite = new FGMembersite();
+
+//Provide your site name here
+$fgmembersite->SetWebsiteName('ihsassassin.netii.net');
+
+//Provide the email address where you want to get notifications
+$fgmembersite->SetAdminEmail('kunaal@live.com');
+
+//Provide your database login details here:
+//hostname, user name, password, database name and table name
+//note that the script will create the table (for example, fgusers in this case)
+//by itself on submitting register.php for the first time
+$fgmembersite->InitDB(/*hostname*/'localhost',
+                      /*username*/'id854024_assassin',
+                      /*password*/'codeclub',
+                      /*database name*/'id854024_studentdirectory',
+                      /*table name*/'Students');
+
+//For better security. Get a random string from this link: http://tinyurl.com/randstr
+// and put it here
+$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+
+?>
